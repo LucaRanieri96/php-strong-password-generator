e8425552b580425ab9e6987f7f34e6cf6182a21e
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-05 15:24:44 +0200

    function generatePassword work in progress

--- index.php
+++ index.php
@@ -10,7 +10,28 @@ $simboli["maiuscole"] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 $simboli["numeri"] = '1234567890';
 $simboli["caratteri_speciali"] = '!?~@#-_+<>[]{}';
 
+// controllo l'array
 var_dump($simboli);
+
+// metto l'input in una variabile
+$passwordLength = $_GET["input"];
+
+// l'idea per generare la password è: prendo l'array di simboli e li mescolo tra di loro, poi da questo array mescolato voglio prendere randomicamente tanti simboli quanti sono quelli richiesti dall'utente e generare così una nuova password
+function generatePassword($passwordLength, $simboli){
+  // mi preparo la stringa per la password
+  $password = "";
+  // mescolo i simboli
+  shuffle($simboli);
+  // ciclo dentro l'array simboli quante volte mi ha detto l'utente
+  for ($i = 0; $i < $passwordLength; $i++) {
+    // mi prendo una chiave random dall'array
+    $key = array_rand($simboli);
+    //ora devo prendermi un carattere random dagli array selezionati dentro i simboli
+    
+    
+}
+}
+
 
 ?>
 
