4810b0483fa1631007155bdfe5360137f42e977f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-05 14:32:09 +0200

    Init index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+var_dump($_GET["input"])
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="" method="get">
+    <div class="input-group mb-3">
+      <input type="number" class="form-control" placeholder="" aria-label="Username"
+        aria-describedby="basic-addon1" name="input">
+      <button type="submit">Submit</button>
+    </div>
+
+  </form>
+</body>
+
+</html>
